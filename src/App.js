336ef6f4ab48c327501a2f89881fb2750567f31f
lamia <Sadki_lamiae@emsi-edu.ma>
2024-07-10 22:37:14 +0100

    this is a normal checkup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import Register from './components/Register';
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // Import your styles if you have any
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
